a6cea36151f9c398c6fc45c6f3312ca9d4be34ab
Andrya_A354 <andrya.afgani@gmail.com>
2022-06-11 22:33:45 +0700

    add authentication using auth-code flow

---
+++ WebClient/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignOut(string page)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -1,7 +1,82 @@
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Net;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+               .AddMicrosoftIdentityWebApp(options =>
+               {
+                   configuration.Bind("AzureAD", options);
+                   options.ResponseType = OpenIdConnectResponseType.Code;
+                   options.Events ??= new OpenIdConnectEvents();
+                   options.Events.OnTokenValidated += OnTokenValidatedFunc;
+                   options.Events.OnAuthorizationCodeReceived += OnAuthorizationCodeReceivedFunc;
+               });
+
+async Task OnAuthorizationCodeReceivedFunc(AuthorizationCodeReceivedContext ctx)
+{
+    try
+    {
+        //string userId = ctx.Principal.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+        //var request = ctx.HttpContext.Request;
+        //var currentUri = UriHelper.BuildAbsolute(request.Scheme, request.Host, request.PathBase, request.Path);
+        //var credential = new ClientCredential(ctx.Options.ClientId, ctx.Options.ClientSecret);
+        //var authContext = new AuthenticationContext(ctx.Options.Authority);
+        //var result = await authContext.AcquireTokenByAuthorizationCodeAsync(
+        //           ctx.ProtocolMessage.Code, new Uri(currentUri), credential, ctx.Options.Resource);
+        //ctx.HandleCodeRedemption(result.AccessToken, result.IdToken);
+
+        //var _authorityAPI = "https://login.microsoftonline.com/7e59cca2-3b49-4008-8ec5-c6558225d6cf";
+        //HttpClient httpClient = new HttpClient();
+        //TokenClientOptions tokenClientOptions = new TokenClientOptions()
+        //{
+        //    ClientId = ctx.Options.ClientId,
+        //    ClientSecret = ctx.Options.ClientSecret,
+        //    Address = string.Format("{0}/oauth2/v2.0/token", _authorityAPI)
+        //};
+        //var tokenClient = new TokenClient(httpClient, tokenClientOptions);
+
+        //var tokenResponse = tokenClient.RequestAuthorizationCodeTokenAsync("c261c512-624d-40fa-90b1-fbdfc5a7c87a", "https://localhost:7167/signin-oidc").Result;
+        //authorizationCtx.HandleCodeRedemption(tokenResponse.AccessToken, tokenResponse.IdentityToken);
+
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+    catch (Exception e)
+    {
+        throw;
+    }
+   
+}
+
+async Task OnTokenValidatedFunc(TokenValidatedContext ctx)
+{
+    await Task.CompletedTask.ConfigureAwait(false);
+}
+
+builder.Services.AddRazorPages()
+    //.AddMvcOptions(options =>
+    //{
+    //    var policy = new AuthorizationPolicyBuilder()
+    //                  .RequireAuthenticatedUser()
+    //                  .Build();
+    //    options.Filters.Add(new AuthorizeFilter(policy));
+    //})
+    .AddMicrosoftIdentityUI();
+
+HttpClient.DefaultProxy = new WebProxy(new Uri("http://localhost:8866"));
 
 var app = builder.Build();
 
@@ -18,6 +93,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
