fa6a174bb78b1bae35c2bd1703ba853a7bc67086
Andrya_A354 <andrya.afgani@gmail.com>
2022-06-22 21:44:35 +0700

    add nullable

--- WebClient/Models/IdentityOptions.cs
+++ WebClient/Models/IdentityOptions.cs
@@ -2,10 +2,10 @@
 {
     public class IdentityOptions
     {
-        public string Instance { get; set; }
-        public string TenantId { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-        public string CallbackPath { get; set; }
+        public string? Instance { get; set; }
+        public string? TenantId { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? CallbackPath { get; set; }
     }
 }
