030385e2b09f480f8aa6a7138e7183ae93219a65
aafgani <andrya.afgani@gmailexample.com>
2022-07-12 21:28:33 +0700

    change authorization level

--- MyFunction04/HelloFunction.cs
+++ MyFunction04/HelloFunction.cs
@@ -16,7 +16,7 @@ namespace MyFunction04
         }
 
         [Function("HelloFunction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
